cf3616e07a95338f75d9d5e8567847342caf16ed
Scott Tolinski <scott@leveluptuts.com>
2022-12-20 10:07:32 -0700

    adds lock and rebuilds pages

--- .svelte-kit/output/server/index.js
+++ .svelte-kit/output/server/index.js
@@ -3145,7 +3145,7 @@ class Server {
       app_template,
       app_template_contains_nonce: false,
       error_template,
-      version: "1671555861009"
+      version: "1671556043721"
     };
   }
   async init({ env }) {
