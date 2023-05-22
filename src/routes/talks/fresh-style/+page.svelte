6c2220c4a6d2944c0064a543055eb115e6de563f
Scott Tolinski <scott@leveluptuts.com>
2023-05-22 13:24:26 -0600

    starts to add links

--- src/routes/talks/fresh-style/+page.svelte
+++ src/routes/talks/fresh-style/+page.svelte
@@ -1,5 +1,46 @@
 <h1>Fresh Style Links</h1>
 
 <ul>
-  <li><a href="#">Links coming soon</a></li>
+  <li>
+    Color
+    <ul>
+      <li>
+        <ul>
+          <li>
+            <a
+              href="https://evilmartians.com/chronicles/oklch-in-css-why-quit-rgb-hsl#oklch-vs-oklab--lch-vs-lab"
+              >OKLCH in CSS</a
+            >
+          </li>
+          <li>
+            <a
+              href="https://developer.chrome.com/articles/high-definition-css-color-guide/"
+              >High Definition CSS Color Guide - Adam Argyle</a
+            >
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </li>
+  <li>
+    Animations
+    <ul>
+      <li>
+        Page Transitions
+        <ul>
+          <li>
+            <a href="https://live-transitions.pages.dev/"
+              >https://live-transitions.pages.dev/</a
+            >
+          </li>
+        </ul>
+      </li>
+      <li>
+        animation-composition demo
+        <a href="https://codepen.io/argyleink/pen/BaPxMWb"
+          >https://codepen.io/argyleink/pen/BaPxMWb</a
+        >
+      </li>
+    </ul>
+  </li>
 </ul>
